7eb5c212afd10b0af5bbd31ea77e196a49e4eb06
roommate6 <floginstefan@gmail.com>
2023-10-30 22:47:44 +0200

    done the image resizer app

--- image-resizer/main.js
+++ image-resizer/main.js
@@ -1,14 +1,26 @@
 const path = require("path");
-const { app, BrowserWindow, Menu } = require("electron");
+const os = require("os");
+const fs = require("fs");
+const resizeImg = require("resize-img");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+
+process.env.NODE_ENV = "production";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 const isMac = process.platform === "darwin";
 
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "Image Resizer",
     width: isDevelopment ? 1000 : 500,
     height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   if (isDevelopment) {
@@ -18,11 +30,25 @@ function createMainWindow() {
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About Image Resizer",
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
 app.whenReady().then(() => {
   createMainWindow();
 
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -32,6 +58,19 @@ app.whenReady().then(() => {
 });
 
 const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
   {
     label: "File",
     submenu: [
@@ -44,7 +83,48 @@ const menu = [
       },
     ],
   },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
 ];
+
+ipcMain.on("image:resize", (e, options) => {
+  options.destination = path.join(os.homedir(), "imageresizer");
+  resizeImage(options);
+});
+
+async function resizeImage({ imagePath, width, height, destination }) {
+  try {
+    const newPath = await resizeImg(fs.readFileSync(imagePath), {
+      width: +width,
+      height: +height,
+    });
+
+    const filename = path.basename(imagePath);
+
+    if (!fs.existsSync(destination)) {
+      fs.mkdirSync(destination);
+    }
+
+    fs.writeFileSync(path.join(destination, filename), newPath);
+
+    mainWindow.webContents.send("image:done");
+
+    shell.openPath(destination);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 app.on("window-all-closed", () => {
   if (!isMac) {
--- image-resizer/renderer/js/renderer.js
+++ image-resizer/renderer/js/renderer.js
@@ -1,24 +1,88 @@
-// Some JavaScript to load the image and show the form. There is no actual backend functionality. This is just the UI
-
-const form = document.querySelector('#img-form');
+const form = document.querySelector("#img-form");
+const img = document.querySelector("#img");
+const outputPath = document.querySelector("#output-path");
+const fileName = document.querySelector("#filename");
+const heightInput = document.querySelector("#height");
+const widthInput = document.querySelector("#width");
 
 function loadImage(e) {
   const file = e.target.files[0];
 
   if (!isFileImage(file)) {
-      alert('Please select an image file');
-        return;
+    alertError("Please select an image!");
+    return;
   }
 
-  form.style.display = 'block';
-  document.querySelector(
-    '#filename'
-  ).innerHTML = file.name;
+  const image = new Image();
+  image.src = URL.createObjectURL(file);
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
+  };
+
+  form.style.display = "block";
+  fileName.innerText = file.name;
+  outputPath.innerText = path.join(os.homedir(), "imageresizer");
 }
+
+function sendImage(e) {
+  e.preventDefault();
+
+  const height = heightInput.value;
+  const width = widthInput.value;
+  const imagePath = img.files[0].path;
+
+  if (!img.files[0]) {
+    alertError("Please upload an image!");
+    return;
+  }
+
+  if (height === "" || width === "") {
+    alertError("Please fill in a height and width!");
+    return;
+  }
+
+  ipcRenderer.send("image:resize", {
+    imagePath,
+    width,
+    height,
+  });
+}
+
+ipcRenderer.on("image:done", () => {
+  alertSuccess(`Image resized to ${widthInput.value} x ${heightInput.value}`);
+});
 
 function isFileImage(file) {
-    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-    return file && acceptedImageTypes.includes(file['type'])
+  const acceptedImageTypes = ["image/gif", "image/png", "image/jpeg"];
+  return file && acceptedImageTypes.includes(file["type"]);
 }
 
-document.querySelector('#img').addEventListener('change', loadImage);
+function alertError(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: "red",
+      color: "white",
+      textAlign: "center",
+    },
+  });
+}
+
+function alertSuccess(message) {
+  Toastify.toast({
+    text: message,
+    duration: 5000,
+    close: false,
+    style: {
+      background: "green",
+      color: "white",
+      textAlign: "center",
+    },
+  });
+}
+
+img.addEventListener("change", loadImage);
+form.addEventListener("submit", sendImage);
