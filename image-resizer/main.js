189f9e1438b36b7632a4afc7e76b3d45fab7d95c
roommate6 <floginstefan@gmail.com>
2023-10-30 20:51:19 +0200

    created the base rendered, now, if we run npm start, it will show hello world

--- image-resizer/main.js
+++ image-resizer/main.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: 500,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+});
