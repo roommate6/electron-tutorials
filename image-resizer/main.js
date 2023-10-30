2ee61416b1e17cfbb007c0973ca2754b4cb63d70
roommate6 <floginstefan@gmail.com>
2023-10-30 21:05:07 +0200

    open devtools when in development environment

--- image-resizer/main.js
+++ image-resizer/main.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+const isMac = process.platform === "darwin";
+
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "Image Resizer",
@@ -8,9 +11,25 @@ function createMainWindow() {
     height: 600,
   });
 
+  if (isDevelopment) {
+    mainWindow.webContents.openDevTools();
+  }
+
   mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
 app.whenReady().then(() => {
   createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
